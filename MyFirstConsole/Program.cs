a7923636f170bc161aa335f69f5843cdc3fbbc12
Hohterd <Hohterd@DESKTOP-M845TE0>
2021-10-11 18:25:56 +0300

    Добавьте файлы проекта.

---
+++ MyFirstConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MySecondConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pi = Convert.ToDecimal(Math.PI);
+            const decimal e = 1.414213562373095048m;
+            Console.WriteLine(pi + " " + e);
+        }
+    }
+}
