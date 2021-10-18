431fb0b3ae99379109e2ad2bf001a313b8a90ef3
Сергей Королёв <hohterd@gmail.com>
2021-10-18 19:25:46 +0300

    homework3 + homework4

---
+++ Homework3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(coneSquare(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine())));
+
+            static double coneSquare(double r, double l)
+            {
+                const double pi = Math.PI;
+                return pi * r * (r + l);
+            }
+
+            int x = 14, y = 1, z = 5;
+            x += y - x++ * z;
+            Console.WriteLine(x);
+            z = --x - y * 5;
+            Console.WriteLine(z);
+            y /= x + 5 % z;
+            Console.WriteLine(y);
+            z = x++ + y * 5;
+            Console.WriteLine(z);
+            x = y - x++ * z;
+            Console.WriteLine(x);
+
+        }
+    }
+}
---
+++ Homework4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int number = int.Parse(Console.ReadLine());
+            if(number >= 0 && number <= 30)
+            {
+                Console.WriteLine($"{number} is in between 0-30 interval");
+            }
+            else if (number >= 31 && number <= 60)
+            {
+                Console.WriteLine($"{number} is in between 31-60 interval");
+            }
+            else if (number >= 61 && number <= 100)
+            {
+                Console.WriteLine($"{number} is in between 61-100 interval");
+            }
+            else
+            {
+                Console.WriteLine("I don't know that number");
+            }
+
+            string numberDivideBy17 = "";
+            int x = 200;
+            while (true)
+            {
+                if (x >= 500) break;
+                if(x%17 == 0)
+                {
+                    numberDivideBy17 += $"{x}, ";
+                }
+                x++;
+            }
+            Console.WriteLine(numberDivideBy17);
+        }
+    }
+}
