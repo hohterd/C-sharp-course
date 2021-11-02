bb074b1921b853914cb0f2d63073f63524c98946
Сергей Королёв <hohterd@gmail.com>
2021-11-02 15:55:26 +0300

    Homework7

--- BookLibraryTest/Program.cs
+++ BookLibraryTest/Program.cs
@@ -7,33 +7,7 @@ namespace BookLibraryTest
     {
         static void Main(string[] args)
         {
-            Book bookOne = new Book("Whom Bell Tolls", 637, "Ernest Miller Hemingway");
-            Book bookTwo = new Book("If it Bleeds", 496);
-
-            Console.WriteLine(Library.AddBookToLibrary(bookOne));
-            Console.WriteLine(Library.AddBookToLibrary(bookTwo));
-
-            /*
-            Console.WriteLine(Library.InformationAboutBookByIndex(0));
-            Console.WriteLine(Library.InformationAboutBookByIndex(1));
-            Console.WriteLine(Library.InformationAboutBookByIndex(-1));
-            Console.WriteLine(Library.InformationAboutBookByIndex(3));
-            */
-
-            /*Console.WriteLine(Library.InformationAboutBookByName("whom bell tolls"));
-            Console.WriteLine(Library.InformationAboutBookByName("WhOM bEll TolLS"));
-            Console.WriteLine(Library.InformationAboutBookByName("If it Bleeds"));
-            Console.WriteLine(Library.InformationAboutBookByName("Flower of Kamaleynyk"));
-            Console.WriteLine(Library.InformationAboutBookByName("qweqweqwef"));*/
-
-            /*Console.WriteLine(Library.DeleteBookFromLibraryByName("Flower of Kamaleynyk"));
-            Console.WriteLine(Library.DeleteBookFromLibraryByIndex(7));
-            Console.WriteLine(Library.DeleteBookFromLibraryByName("WhOM bEll TolLS"));
-            Console.WriteLine(Library.DeleteBookFromLibraryByIndex(1));
-            Console.WriteLine(Library.DeleteBookFromLibraryByName("WhOM bEll TolLS"));
-            Console.WriteLine(Library.DeleteBookFromLibraryByIndex(1));*/
-
-            Console.WriteLine(Library.FindTheBiggestOneBook(Library.Books));
+            
         }
     }
 }
---
+++ CustomClassesLibrary/ArraySort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace CustomClassesLibrary
+{
+    public class ArraySort
+    {
+
+        public int[] GetReverseSortArray(int[] array)
+        {
+            Array.Sort(array);
+            Array.Reverse(array);
+            return array;
+        }
+
+        public int[] GetMaxValueEachRow(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            int[] result = new int[rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int max = array[i, 0];
+                for (int j = 0; j < columns; j++)
+                {
+                    if (max < array[i, j])
+                    {
+                        max = array[i, j];
+                    }
+                }
+                result[i] = max;
+            }
+            return result;
+        }
+    }
+}
