7f07dba861ac0af4b00e2d78e6f7f7bfa446d0ba
Сергей Королёв <hohterd@gmail.com>
2021-12-05 18:44:13 +0300

    Homework14

---
+++ CustomClassesLibrary/Homework14/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CustomClassesLibrary.Homework14
+{
+    public class Person
+    {
+        [PersonValidation(@"^(?:1[8-9]|[2-9][0-9]|1[01][0-9])$", ErrorMessage = "U must be older 18")]
+        public int Age { get; set;}
+        [PersonValidation(@"^([A-Z]{1}[A-Za-z]{1,40})$", ErrorMessage = "First name should begin with Uppercase char and not longer than 40 chars")]
+        public string FirstName { get; set; }
+        [PersonValidation(@"^([A-Z]{1}[A-Za-z]{1,40})$", ErrorMessage = "Last name should begin with Uppercase char and not longer than 40 chars")]
+        public string LastName { get; set; }
+
+        public Person(int age, string firstName, string lastName)
+        {
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public bool IsValid()
+        {
+            Type item = this.GetType();
+
+            PropertyInfo[] properties = item.GetProperties(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (PropertyInfo property in properties)
+            {
+                foreach (var attr in property.GetCustomAttributes(false))
+                {
+                    var temp = attr as PersonValidationAttribute;
+                    if (temp != null && !temp.IsValid(property.GetValue(this)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CustomClassesLibrary/Homework14/PersonValidationAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CustomClassesLibrary.Homework14
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class PersonValidationAttribute : RegularExpressionAttribute
+    {
+        public PersonValidationAttribute(string pattern) : base(pattern)
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                var regexValidator = new RegularExpressionAttribute(Pattern);
+                if (!regexValidator.IsValid(value))
+                {
+                    Console.WriteLine(ErrorMessage);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+    }
+}
